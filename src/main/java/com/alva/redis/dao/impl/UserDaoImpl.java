5e640e25c7eb264004dcff4c43bd0426a506cd67
daisyding <qi_ding@126.com>
2016-01-13 16:49:35 +0800

    fix

--- src/main/java/com/alva/redis/dao/impl/UserDaoImpl.java
+++ src/main/java/com/alva/redis/dao/impl/UserDaoImpl.java
@@ -12,6 +12,8 @@ import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by alva on 16/1/5.
@@ -30,14 +32,14 @@ public class UserDaoImpl implements  UserDao {
 
     public void addUser(String userName, String userPwd) {
 
-       /* User user = new User(userName,userPwd);
-        listOps.leftPush(userName, SerializeUtil.serialize(user));
-        List<byte[] > ll= listOps.range(userName,0,-1);
+        User user = new User(userName,userPwd);
+        listOps.leftPush(userName, JSONObject.toJSONString(user));
+        List<String> ll= listOps.range(userName,0,-1);
         System.out.println("ll.size : "+ll.size());
         for(int i=0;i<ll.size();i++){
-            User u =(User)SerializeUtil.unserialize(ll.get(i));
+            User u =(User)JSONObject.parseObject(ll.get(i),User.class);
             System.out.println("user.name : "+u.getName());
-        }*/
+        }
 
     }
 
@@ -65,17 +67,15 @@ public class UserDaoImpl implements  UserDao {
             @Override
             public User execute(RedisOperations operations) throws DataAccessException {
                 operations.multi();
+                //事务,将多个命令排队执行.
                 String key = myKey + ":" + myUser.getName();
                 operations.opsForHash().put(key, "1", JSONObject.toJSONString(myUser));
-                //operations.expire(key, 6, TimeUnit.SECONDS);
-
-
-                /*
+                operations.expire(key, 6, TimeUnit.SECONDS);
                 BoundValueOperations<String, String> oper = operations.boundValueOps(key);
                 oper.set(myUser.getPwd());
                 oper.expire(5, TimeUnit.SECONDS);
                 oper.set("second");//这里再执行一次
-                */
+
                 operations.exec();
                 System.out.println(" after  addUserTx ");
                 User  u = JSONObject.parseObject(operations.opsForHash().get(key, "1").toString(), User.class);
